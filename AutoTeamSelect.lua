a7c4e3ac8fce5936ceddffca144974f6cc40d436
Carter Poe <39221871+Carter-Poe@users.noreply.github.com>
2022-07-24 22:02:47 -0400

    Update AutoTeamSelect.lua

--- AutoTeamSelect.lua
+++ AutoTeamSelect.lua
@@ -6,7 +6,6 @@ local lastValueTeam = autoTeam:GetValue()
 
 local function updateTeam()
 	local LocalPlayer = entities.GetLocalPlayer()
-	
 	if autoTeam:GetValue() == 0 then
 		if LocalPlayer:GetTeamNumber() ~= 2 then
 			client.Command("jointeam 2", false)
@@ -19,10 +18,22 @@ local function updateTeam()
 	end
 end
 
+local timer = 0
+
 callbacks.Register("Draw", function()
+	timer = timer + 1
+	print(timer)
+	if timer >= 700 then
+		if entities.GetLocalPlayer() ~= nil then
+			updateTeam()
+		end
+		timer = 0
+	end
 	if lastValueTeam ~= autoTeam:GetValue() then
 		lastValueTeam = autoTeam:GetValue()
-		updateTeam()
+		if entities.GetLocalPlayer() ~= nil then
+			updateTeam()
+		end
 	end
 end)
 
@@ -37,8 +48,16 @@ callbacks.Register("FireGameEvent", function(event)
 	if event:GetName() == "jointeam_failed" then updateTeam() end
 	if event:GetName() == "player_death" then updateTeam() end
 	if event:GetName() == "cs_win_panel_round" then updateTeam() end
+	if event:GetName() == "switch_team" then updateTeam() end
+	if event:GetName() == "player_become_ghost" then updateTeam() end
+	if event:GetName() == "player_spawned" then updateTeam() end
+	if event:GetName() == "cs_intermission" then updateTeam() end
 end)
 
+client.AllowListener("cs_intermission")
+client.AllowListener("player_spawned")
+client.AllowListener("player_become_ghost")
+client.AllowListener("switch_team")
 client.AllowListener("cs_win_panel_round")
 client.AllowListener("player_death")
 client.AllowListener("jointeam_failed")
